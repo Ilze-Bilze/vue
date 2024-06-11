ace07f36ed1a6dcb7352f14d0cf1fb5aa2da1303
Hero <hero@Heros-iMac.local>
2024-06-11 17:53:56 +0300

    learning about dev tools, data, multiple vue instances, accessing the instances, methods, directives, two-way data binding, binding attributes, outputing raw

--- section-2-1-COMPLETE/app.js
+++ section-2-1-COMPLETE/app.js
@@ -1,7 +1,28 @@
-Vue.createApp({
+const vm = Vue.createApp({
   data() {
     return {
-      firstName: 'John'
+      firstName: 'John',
+      lastName: 'Smith',
+      url: 'https://google.com',
+      raw_url: '<a href="https://google.com" target="_blank">Google</a>'
+    }
+  },
+  methods: {
+    fullName() {
+      return `${this.firstName} ${this.lastName.toUpperCase()}`
     }
   }
 }).mount('#app')
+
+setTimeout(() => {
+  vm.firstName = 'Bob';
+}, 2000)
+
+// Vue.createApp({
+//   data() {
+//     return {
+//       firstName: 'Paul',
+//       lastName: 'Doe'
+//     }
+//   }
+// }).mount('#appId')
