6d48d8755e954161a86cac7e22f0b4315691a647
Hero <hero@Heros-iMac.local>
2024-06-12 14:34:12 +0300

    added Listening to Events  Passing on Data with Events Event Modifiers Keyboard Events and Modifiers V-model Modifiers  Computed Properties Watchers Binding Classes Binding Styles

--- section-2-1-COMPLETE/app.js
+++ section-2-1-COMPLETE/app.js
@@ -1,15 +1,43 @@
 const vm = Vue.createApp({
+  // Use simly for data
   data() {
     return {
       firstName: 'John',
+      middleName: '',
       lastName: 'Smith',
       url: 'https://google.com',
-      raw_url: '<a href="https://google.com" target="_blank">Google</a>'
+      raw_url: '<a href="https://google.com" target="_blank">Google</a>',
+      age: 20
     }
   },
+  // Use for functions
   methods: {
+    increment() {
+      this.age++
+    },
+    updateLastName(msg, event) {
+      console.log(msg)
+      this.lastName = event.target.value
+    },
+    updateMiddleName(event) {
+      this.middleName = event.target.value
+    }
+  },
+  // Use for calculations 
+  // https://vuejs.org/guide/essentials/computed.html#computed-properties
+  // Computed properties can never be synchronous
+  computed: {
     fullName() {
-      return `${this.firstName} ${this.lastName.toUpperCase()}`
+      console.log('Full name computed property was called!')
+
+      return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
+    }
+  },
+  watch: {
+    age(newVal, oldVal) {
+      setTimeout(() => {
+        this.age = 20
+      }, 3000)
     }
   }
 }).mount('#app')
